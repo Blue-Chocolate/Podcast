10014542390700870a7519cdce449acbc306c070
Blue-chocolate <haasklany@gmail.com>
2025-10-20 12:51:11 +0300

    File size in liverwire

--- config/livewire.php
+++ config/livewire.php
@@ -2,15 +2,14 @@
 
 return [
     'temporary_file_upload' => [
-        'disk' => 'livewire-tmp',
-        'rules' => null,
-        'directory' => null,
-        'middleware' => null,
-        'preview_mimes' => [
-            'png', 'gif', 'bmp', 'svg', 'wav', 'mp3', 
-            'mp4', 'mov', 'avi', 'wmv', 'webm', 'ogg',
-            'jpg', 'jpeg', 'pdf', 'm4a',
-        ],
-        'max_upload_time' => 120, 
+    'disk' => 'livewire-tmp',
+    'rules' => 'max:10240', // الحجم بالـ KB، هنا 10240KB = 10MB
+    'preview_mimes' => [
+        'png', 'gif', 'bmp', 'svg', 'wav', 'mp3', 
+        'mp4', 'mov', 'avi', 'wmv', 'webm', 'ogg',
+        'jpg', 'jpeg', 'pdf', 'm4a',
     ],
+    'max_upload_time' => 120, 
+],
+
 ];
